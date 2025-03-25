91ba0809c57a2fe6cad8d5db3b6d07066306802c
wheeler-cs <wheelercs@protonmail.com>
2025-03-25 12:16:30 -0500

    Some More Asynch Stuff
    Workin some more on the asynchronous part of the server, mainly with
    thread handling.
    
    Signed-off-by: wheeler-cs <wheelercs@protonmail.com>

--- src/Server.py
+++ src/Server.py
@@ -9,39 +9,54 @@ from typing import List
 
 # ======================================================================================================================
 class Server(object):
-    '''
-    
+    '''Asynchronous, multi-connection socket server.
+
+    Attributes:
+        connection (socket.socket): Server-side connection listening for new clients.
+        port (int): Port on which server is set to listen.
+
+        max_conns (int): Maximum number of clients able to connect to server.
+        conn_list (List[threading.Thread]): List of threads containing connections with clients.
     '''
     def __init__(self, cfg_file: str = "cfg/core.json") -> None:
-        '''
+        '''Server class initializer.
+
+        Initializes all member variables and binds server listening connection to desired port.
+
+        Args:
+            cfg_file (str): Path to file containing configuration information for the server.
         
         '''
         # Connection information
         self.connection = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.port:         int = Configuration.DEFAULT_PORT
+        self.connection.bind(('', self.port))
         # Async management
-        self.active_conns: int                    = 0
         self.max_conns:    int                    = Configuration.DEFAULT_MAX_CLIENTS
         self.conn_list:    List[threading.Thread] = list()
-        self.initialize()
+        self.collect:      threading.Thread       = threading.Thread(target=self.thread_collect)
+        self.collect.start()
 
 
 
     def load_cfg(self, cfg_file: str) -> None:
-        # TODO: Implement this
+        '''
+        TODO: Implement this
+        '''
         cfg_data = None
         with open(cfg_file, 'r') as cfg_read:
             cfg_data = json.load(cfg_read)
         print(cfg_data)
 
 
-    def initialize(self) -> None:
-        self.connection.bind(('', self.port))
-        print(f"[INFO] Server initialized")
+    def async_conn(self, conn: socket.socket, addr) -> None:
+        '''Asynchronous connection running in own thread upon successful connection with client.
 
-    
-    def async_conn(self, conn, addr) -> None:
-        print(f"[INFO] Established connection with {addr} ({self.active_conns}/{self.max_conns})")
+        Args:
+            conn (socket.socket): Connection object streaming data between client and server.
+            addr (socket._RetAddress): Return address for client.
+        '''
+        print(f"[INFO] Established connection with {addr} ({len(self.conn_list)}/{self.max_conns})")
         while True:
             data = conn.recv(2048)
             dec_data = data.decode()
@@ -53,7 +68,19 @@ class Server(object):
                 print(f"{addr}: {dec_data}")
             conn.send(data)
         conn.close()
-        self.active_conns -= 1
+    
+
+    def thread_collect(self):
+        '''
+        
+        '''
+        print("[INFO] Watching for dead threads")
+        # Reap zombie threads
+        for thread in self.conn_list:
+            thread.join(0.5)
+            if(not(thread.is_alive())):
+                print(f"Reaping thread: {thread}")
+                self.conn_list.remove(thread)
 
 
     def run(self) -> None:
@@ -62,9 +89,8 @@ class Server(object):
         while True:
             # Listen for connection attempts
             conn, addr = self.connection.accept()
-            if(self.active_conns < self.max_conns):
+            if(len(self.conn_list) < self.max_conns):
                 self.conn_list.append(threading.Thread(target=self.async_conn, args=(conn, addr)))
-                self.active_conns += 1
                 self.conn_list[-1].start()
             else: # Connections maxxed; clear buffer and send BLOCK
                 data = conn.recv(2048)
